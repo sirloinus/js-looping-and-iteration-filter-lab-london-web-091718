cd7a0202084011a525002d7a2b839cba45147989
Amalie Smidth <amaliesmidth@gmail.com>
2018-10-29 18:15:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file
+function findMatching(drivers, name){
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, letters){
+  let nameLength = letters.length
+  return drivers.filter(driver => driver.slice(0, nameLength) === letters)
+}
+
+function matchName(drivers, name){
+  return drivers.filter(driver => driver.name === name)
+}
